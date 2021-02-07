4989b0efaf303abfe87d3b71e9669c7d41babcdd
name <email>
2021-02-07 15:46:13 +0200

    fix wc_client_on_resize()

--- src/wc_client_on_resize.c
+++ src/wc_client_on_resize.c
@@ -6,7 +6,7 @@
 /*   By: ***REMOVED*** <***REMOVED***@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/27 11:38:12 by ***REMOVED***          #+#    #+#             */
-/*   Updated: 2021/01/27 11:41:49 by ***REMOVED***         ###   ########.fr       */
+/*   Updated: 2021/02/07 15:43:58 by ***REMOVED***         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,14 @@
 
 void	wc_client_on_resize(t_client *c, t_s32 width, t_s32 height)
 {
+	SDL_DestroyTexture(c->texture);
+	if (!(c->texture = SDL_CreateTexture(c->renderer, SDL_PIXELFORMAT_RGBA8888,
+		SDL_TEXTUREACCESS_STREAMING, width, height)))
+	{
+		c->run = wx_false;
+	}
 	free(c->frame_buffer.data);
-	c->frame_buffer.data = 0;
+	c->frame_buffer.data = WX_NULL;
 	c->frame_buffer.data_size = width * height * 4;
 	if (!(c->frame_buffer.data = (t_u8 *)malloc(c->frame_buffer.data_size)))
 	{
