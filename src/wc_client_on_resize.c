97b9771b61ba303581a7028b81748521b329a029
name <email>
2021-02-08 14:57:32 +0200

    fix wc_client_on_resize()

--- src/wc_client_on_resize.c
+++ src/wc_client_on_resize.c
@@ -6,15 +6,23 @@
 /*   By: ***REMOVED*** <***REMOVED***@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/27 11:38:12 by ***REMOVED***          #+#    #+#             */
-/*   Updated: 2021/02/07 15:43:58 by ***REMOVED***         ###   ########.fr       */
+/*   Updated: 2021/02/08 14:54:25 by ***REMOVED***         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "wc_client.h"
 
+/*
+** SDL_DestroyRenderer() frees related textures
+*/
+
 void	wc_client_on_resize(t_client *c, t_s32 width, t_s32 height)
 {
-	SDL_DestroyTexture(c->texture);
+	SDL_DestroyRenderer(c->renderer);
+	if (!(c->renderer = SDL_CreateRenderer(c->window, -1, 0)))
+	{
+		c->run = wx_false;
+	}
 	if (!(c->texture = SDL_CreateTexture(c->renderer, SDL_PIXELFORMAT_RGBA8888,
 		SDL_TEXTUREACCESS_STREAMING, width, height)))
 	{
@@ -28,4 +36,6 @@ void	wc_client_on_resize(t_client *c, t_s32 width, t_s32 height)
 		c->run = wx_false;
 	}
 	wx_buffer_set(c->frame_buffer.data, c->frame_buffer.data_size, 0);
+	c->frame_buffer.width = width;
+	c->frame_buffer.height = height;
 }
