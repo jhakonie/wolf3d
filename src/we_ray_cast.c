fc3b64c564360273647430ca81e136ecf48b9cca
name <email>
2021-03-23 16:52:13 +0200

    fix linux build, extranous unsigned >= 0 checks

--- src/we_ray_cast.c
+++ src/we_ray_cast.c
@@ -6,7 +6,7 @@
 /*   By: jhakonie <jhakonie@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/15 17:25:44 by jhakonie          #+#    #+#             */
-/*   Updated: 2021/03/23 00:59:42 by jhakonie         ###   ########.fr       */
+/*   Updated: 2021/03/23 16:51:06 by ***REMOVED***         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,14 +25,13 @@ static t_bool		zz_is_wall(t_item *chart, t_ray *ray,
 	block_y = intersection.y / WE_BLOCK_W;
 	block_x = intersection.x / WE_BLOCK_W;
 	index = block_x + block_y * WE_GRID_DIVIDE;
-	if (block_x < 0 || block_x >= WE_GRID_DIVIDE ||
-			block_y < 0 || block_y >= WE_GRID_DIVIDE)
+	if (block_x >= WE_GRID_DIVIDE || block_y >= WE_GRID_DIVIDE)
 		return (wx_false);
 	if (ray->angle_d <= 180 && ray->angle_d >= 0 && line == WE_HORIZONTAL)
 		index -= WE_GRID_DIVIDE;
 	else if (ray->angle_d >= 90 && ray->angle_d <= 270 && line == WE_VERTICAL)
 		index -= 1;
-	if (index >= 0 && index < block_count - 1)
+	if (index < block_count - 1)
 	{
 		if ((we_ray_cast_check_map(chart, ray, line, index)))
 			return (wx_true);
@@ -59,13 +58,13 @@ static t_f32		zz_dist_horizontal_wall(t_ray *ray, t_item *chart)
 	if (ray->angle_d >= 180 && ray->angle_d <= 360)
 		block_y += 1;
 	intersection.y = block_y * WE_BLOCK_W;
-	while (block_y < WE_GRID_DIVIDE && block_y >= 0)
+	while (block_y < WE_GRID_DIVIDE)
 	{
 		block_y = intersection.y / WE_BLOCK_W;
 		intersection.x = (intersection.y - ray->b) / ray->k;
 		if (intersection.x < 0 || intersection.x >= world_end_x ||
-			(ray->angle_d == 180 || ray->angle_d == 0 || ray->angle_d == 360)
-		|| block_y < 0 || block_y >= WE_GRID_DIVIDE)
+			(ray->angle_d == 180 || ray->angle_d == 0 || ray->angle_d == 360) ||
+			block_y >= WE_GRID_DIVIDE)
 			break ;
 		if ((zz_is_wall(chart, ray, intersection, WE_HORIZONTAL)))
 			return (zz_distance_to_wall(ray->start, intersection));
@@ -88,13 +87,13 @@ static t_f32		zz_dist_vertical_wall(t_ray *ray, t_item *chart)
 	if (ray->angle_d <= 90 || ray->angle_d >= 270)
 		block_x += 1;
 	intersection.x = block_x * WE_BLOCK_W;
-	while (block_x < WE_GRID_DIVIDE && block_x >= 0)
+	while (block_x < WE_GRID_DIVIDE)
 	{
 		block_x = intersection.x / WE_BLOCK_W;
 		intersection.y = ray->k * intersection.x + ray->b;
 		if (intersection.y < 0 || intersection.y >= world_end_y ||
-			(ray->angle_d == 90 || ray->angle_d == 270)
-		|| block_x < 0 || block_x >= WE_GRID_DIVIDE)
+			(ray->angle_d == 90 || ray->angle_d == 270) ||
+			block_x >= WE_GRID_DIVIDE)
 			break ;
 		if ((zz_is_wall(chart, ray, intersection, WE_VERTICAL)))
 			return (zz_distance_to_wall(ray->start, intersection));
